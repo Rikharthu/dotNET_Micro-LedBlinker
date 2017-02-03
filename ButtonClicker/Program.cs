f4f1c16331318a015cc32560cdbd2aa821ef7ba4
UberV <UberV@DESKTOP-905HK19>
2017-02-03 09:51:04 +0200

    Add project files.

---
+++ ButtonClicker/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using GHIElectronics.NETMF.FEZ;
+using System.Threading;
+
+namespace ButtonClicker
+{
+    public class Program
+    {
+
+        public static void Main()
+        {
+            // Izveido ieejas portu uz norādītās CPU kājas 
+            InputPort button = new InputPort(
+                (Cpu.Pin)FEZ_Pin.Digital.LDR,
+                false, // Trokšņu filtrs: false -> izslēgts, true -> ieslēgts 
+                Port.ResistorMode.PullUp); // Uzstādīt kāju uz high, kad nekas nav pievienots 
+
+            OutputPort led = new OutputPort(
+                (Cpu.Pin)FEZ_Pin.Digital.LED, 
+                false);
+
+            bool oldButton=false;
+
+            while (true)
+            {
+                // Nolasīt kājas stāvokli : -true, high, false -> low Kad LDR poga ir nospiesta,
+                // tā savieno LDR kāju ar zemi, iestatot to uz low. 
+                bool buttonPressed = !button.Read();
+                bool buttonToggled = buttonPressed && buttonPressed != oldButton;
+                if (buttonToggled)
+                {
+                    led.Write(!led.Read());
+                    Debug.Print("Button Toggled");
+                }
+                //if (buttonPressed)
+                //{
+                //    // toggle led
+                //    led.Write(!led.Read());
+                //    Debug.Print("Button pressed");
+                //    //Thread.Sleep(50);
+                //}
+                Thread.Sleep(100);
+                Debug.Print("buttonPressed: " + buttonPressed);
+                oldButton = buttonPressed;
+            }
+        }
+    }
+}
---
+++ LedBlinker/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using GHIElectronics.NETMF.FEZ;
+using System.Threading;
+
+namespace LedBlinker
+{
+    public class Program
+    {
+        public const int MAX_DELAY = 1000;
+        public const int MIN_DELAY = 0;
+        public const int DELTA_DELAY = 20;
+
+        private static int delay = MAX_DELAY;
+
+
+        public static void Main()
+        {
+            // Izveido izejas portu uz atbilstošo CPU “kāju” (pin) 
+            OutputPort led = new OutputPort(
+                (Cpu.Pin)FEZ_Pin.Digital.LED, // Ražotāja specificētais  CPU kājas numurs 
+                false); // Sākotnejais stāvoklis: - false -> low (izslēgts) - true -> high (ieslēgts) 
+
+            while (true)
+            {
+                Debug.Print("Delay: " + delay);
+                // Iestatīt kāju uz high 
+                led.Write(true);
+                Thread.Sleep(delay);
+                // Iestatīt kāju uz low 
+                led.Write(false);
+                Thread.Sleep(delay);
+                if (delay > MIN_DELAY)
+                {
+                    delay -= DELTA_DELAY;
+                }
+                else
+                {
+                    Debug.Print("KA-BOOM!");
+                    delay = MAX_DELAY;
+                }
+            }
+        }
+    }
+}
